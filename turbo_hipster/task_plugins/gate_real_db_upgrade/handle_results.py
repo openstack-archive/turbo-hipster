39a0feef7b49f223ef04b0d0416aca6a47598584
Joshua Hesketh <josh@nitrotech.org>
2013-07-31 12:00:53 +1000

    add in copyright

--- turbo_hipster/task_plugins/gate_real_db_upgrade/handle_results.py
+++ turbo_hipster/task_plugins/gate_real_db_upgrade/handle_results.py
@@ -1,13 +1,22 @@
-# Copyright ...
+# Copyright 2013 Rackspace Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 
 """ Methods to handle the results of the task.
 
 Primarily place the log files somewhere useful and optionally email
 somebody """
-
-def push_file(local_file):
-    """ Push a log file to a server. Returns the public URL """
-    pass
 
 def generate_log_index(logfiles):
     """ Create an index of logfiles and links to them """
