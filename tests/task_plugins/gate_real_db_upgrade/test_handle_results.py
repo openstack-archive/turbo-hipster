ef72b8a39f7e5b4f6be4b516eaaf73785a5c447c
Joshua Hesketh <josh@nitrotech.org>
2013-08-12 11:17:14 +1000

    Generate tests to write

---
+++ tests/task_plugins/gate_real_db_upgrade/test_handle_results.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestGenerateLogIndex(unittest.TestCase):
+    def test_generate_log_index(self):
+        # self.assertEqual(expected, generate_log_index(datasets))
+        assert False # TODO: implement your test here
+
+class TestMakeIndexFile(unittest.TestCase):
+    def test_make_index_file(self):
+        # self.assertEqual(expected, make_index_file(datasets))
+        assert False # TODO: implement your test here
+
+class TestGeneratePushResults(unittest.TestCase):
+    def test_generate_push_results(self):
+        # self.assertEqual(expected, generate_push_results(datasets))
+        assert False # TODO: implement your test here
+
+class TestCheckLogForErrors(unittest.TestCase):
+    def test_check_log_for_errors(self):
+        # self.assertEqual(expected, check_log_for_errors(logfile))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_worker_manager.py
@@ -0,0 +1,29 @@
+import unittest
+
+class TestGearmanManager(unittest.TestCase):
+    def test___init__(self):
+        # gearman_manager = GearmanManager(config, tasks)
+        assert False # TODO: implement your test here
+
+    def test_run(self):
+        # gearman_manager = GearmanManager(config, tasks)
+        # self.assertEqual(expected, gearman_manager.run())
+        assert False # TODO: implement your test here
+
+    def test_setup_gearman(self):
+        # gearman_manager = GearmanManager(config, tasks)
+        # self.assertEqual(expected, gearman_manager.setup_gearman())
+        assert False # TODO: implement your test here
+
+    def test_stop(self):
+        # gearman_manager = GearmanManager(config, tasks)
+        # self.assertEqual(expected, gearman_manager.stop())
+        assert False # TODO: implement your test here
+
+    def test_stopped(self):
+        # gearman_manager = GearmanManager(config, tasks)
+        # self.assertEqual(expected, gearman_manager.stopped())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
