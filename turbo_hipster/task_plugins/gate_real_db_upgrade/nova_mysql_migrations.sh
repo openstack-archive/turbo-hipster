e047dc11dd0765ff50b71090b9cf427de44391c6
Joshua Hesketh <josh@nitrotech.org>
2013-08-15 10:57:46 +1000

    fix pip cache dir

--- turbo_hipster/task_plugins/gate_real_db_upgrade/nova_mysql_migrations.sh
+++ turbo_hipster/task_plugins/gate_real_db_upgrade/nova_mysql_migrations.sh
@@ -78,7 +78,7 @@ echo "$0 $1 $2 $3 $4 $5 $6 $7 $8 $9"
 
 # Setup the environment
 export PATH=/usr/lib/ccache:$PATH
-export PIP_DOWNLOAD_CACHE=$8
+export PIP_DOWNLOAD_CACHE=$9
 
 # Restore database to known good state
 echo "Restoring test database $6"
