7d1efae4e01a6c71a30e3fca21d9a9dfe7ee5062
Michael Still <mikal@stillhq.com>
2014-01-01 18:05:37 +1100

    Regenerate historical data, add auto generated data config
    Change-Id: I04a2d7117338dad49e84b7d7d91c81ed405e6674

--- turbo_hipster/report_historical.py
+++ turbo_hipster/report_historical.py
@@ -16,60 +16,117 @@
 
 
 import json
+import math
 import numpy
 import os
 import sys
 
 
 def main():
+    for dataset in ['devstack_131007', 'devstack_150', 'trivial_500',
+                    'trivial_6000', 'user_001', 'user_002']:
+        process_dataset(dataset)
+
+
+def process_dataset(dataset):
     with open('results.json') as f:
         results = json.loads(f.read())
 
     migrations = {}
+    all_times = {}
+
     for engine in ['mysql', 'percona']:
         print
+        print 'Dataset: %s' % dataset
         print 'Engine: %s' % engine
         print
 
-        for migration in sorted(results[engine]['user_001']):
+        for migration in sorted(results[engine][dataset]):
             times = []
-            for time in results[engine]['user_001'][migration]:
-                for i in range(results[engine]['user_001'][migration][time]):
+            all_times.setdefault(migration, [])
+            for time in results[engine][dataset][migration]:
+                for i in range(results[engine][dataset][migration][time]):
                     times.append(int(time))
+                    all_times[migration].append(int(time))
+                    
             times = sorted(times)
+            emit_summary(engine, times, migrations, migration)
 
-            np_times = numpy.array(times)
-            mean = np_times.mean()
-            stddev = np_times.std()
-            failed_threshold = int(max(30.0, mean + stddev * 2))
-
-            failed = 0
-            for time in times:
-                if time > failed_threshold:
-                    failed += 1
-
-            migrations.setdefault(migration, {})
-            migrations[migration][engine] = ('%.02f;%0.2f;%.02f'
-                                             % (mean - 2 * stddev,
-                                                mean,
-                                                mean + 2 * stddev))
-
-            if failed_threshold != 30 or failed > 0:
-                print ('%s: Values range from %s to %s seconds. %d values. '
-                       'Mean is %.02f, stddev is %.02f.\n    '
-                       'Recommend max of %d. With this value %.02f%% of tests '
-                       'would have failed.'
-                       % (migration, np_times.min(), np_times.max(),
-                          len(times), mean, stddev, failed_threshold,
-                          failed * 100.0 / len(times)))
+    print
+    print 'Dataset: %s' % dataset
+    print 'Engine: combined'
+    print
+    for migration in sorted(all_times.keys()):
+        emit_summary('combined', all_times[migration], migrations, migration)
 
     with open('results.txt', 'w') as f:
         f.write('Migration,mysql,percona\n')
         for migration in sorted(migrations.keys()):
             f.write('%s' % migration)
             for engine in ['mysql', 'percona']:
-                f.write(',%s' % migrations[migration][engine])
+                f.write(',%s' % migrations[migration].get(engine, ''))
             f.write('\n')
+
+    # Write out the dataset config as a json blob
+    config_path = os.path.join('datasets',
+                               'datasets_%s' % dataset,
+                               omg_hard_to_predict_names(dataset))
+    with open(os.path.join(config_path, 'input.json')) as f:
+        config = json.loads(f.read())
+
+    for migration in sorted(all_times.keys()):
+        minimum, mean, maximum, stddev = analyse(all_times[migration])
+        recommend = mean + 2 * stddev
+        if recommend > 30.0:
+            config['maximum_migration_times'][migration] = math.ceil(recommend)
+
+    with open(os.path.join(config_path, 'config.json'), 'w') as f:
+        f.write(json.dumps(config, indent=4, sort_keys=True))
+
+
+def omg_hard_to_predict_names(dataset):
+    if dataset.startswith('trivial'):
+        return 'nova_%s' % dataset
+    if dataset == 'devstack_150':
+        return 'datasets_devstack_150'
+    if dataset == 'devstack_131007':
+        return '131007_devstack_export'
+    return dataset
+
+
+def analyse(times):
+    np_times = numpy.array(times)
+    minimum = np_times.min()
+    mean = np_times.mean()
+    maximum = np_times.max()
+    stddev = np_times.std()
+    return minimum, mean, maximum, stddev
+
+
+def emit_summary(engine, times, migrations, migration):
+    minimum, mean, maximum, stddev = analyse(times)
+    failed_threshold = int(max(30.0, mean + stddev * 2))
+
+    failed = 0
+    for time in times:
+        if time > failed_threshold:
+            failed += 1
+
+    migrations.setdefault(migration, {})
+    migrations[migration][engine] = ('%.02f;%0.2f;%.02f'
+                                     % (mean - 2 * stddev,
+                                        mean,
+                                        mean + 2 * stddev))
+
+    if failed_threshold != 30 or failed > 0:
+        print ('%s: Values range from %s to %s seconds. %d values. '
+               'Mean is %.02f, stddev is %.02f.\n    '
+               'Recommend max of %d. With this value %.02f%% of tests '
+               'would have failed.'
+               % (migration, minimum, maximum,
+                  len(times), mean, stddev, failed_threshold,
+                  failed * 100.0 / len(times)))
+
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath(
