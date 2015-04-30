4c0fbd8d25f929550fc413993a13ed12f4063565
Joshua Hesketh <josh@nitrotech.org>
2015-04-30 11:08:59 +1000

    Fix mysql counters for migrate_flavor_data
    Change-Id: Ibfbe082ccbbfcb0826c6805c83745daece27389e

--- turbo_hipster/task_plugins/real_db_upgrade/nova_mysql_migrations.sh
+++ turbo_hipster/task_plugins/real_db_upgrade/nova_mysql_migrations.sh
@@ -122,6 +122,8 @@ EOF
     then
       set -x
       sudo /sbin/ip netns exec nonet `dirname $0`/nova-manage-wrapper.sh $VENV_PATH --config-file $WORKING_DIR_PATH/nova-$1.conf --verbose db migrate_flavor_data --force
+      echo "MySQL counters after migrate_flavor_data:"
+      mysql -u $DB_USER --password=$DB_PASS $DB_NAME -e "show status like 'innodb%';"
       set +x
     fi
 
